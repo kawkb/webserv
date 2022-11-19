3684fc1cebd478c1825e5cf7363bac8046a1a65d
kawkb <dfi3lp@gmail.com>
2022-11-19 03:22:51 +0100

    ok

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: moerradi <moerradi@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: kdrissi- <kdrissi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 15:14:24 by kdrissi-          #+#    #+#             */
-/*   Updated: 2022/11/17 22:39:34 by moerradi         ###   ########.fr       */
+/*   Updated: 2022/11/19 03:17:52 by kdrissi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ int     main(int ac, char **av)
 		std::cerr << "Bad Arguments!" << std::endl;
 		return(1);
 	}
-	std::vector<Server>			servers;
-	std::vector<TcpListener>	tcpListeners;
+	Servers			servers;
+	TcpListeners	tcpListeners;
 	if (parse_config_file(av[1], servers))
 		return(1);
 	// fix last } in configfile.
-	// add cgi
 	if (run_server(servers, tcpListeners))
 		return(1);
 	return(0);
 }
+//
