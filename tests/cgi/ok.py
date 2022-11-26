e1d8af07ad3fc5d98dab64ebdd9f1a0653c94b21
moerradi <erradi1337@gmail.com>
2022-11-26 06:55:59 +0100

    added hello

--- tests/cgi/ok.py
+++ tests/cgi/ok.py
@@ -5,4 +5,7 @@ from time import sleep
 # Create instance of FieldStorage 
 form = cgi.FieldStorage() 
 # get query string
- values = 
+values = cgi.parse()
+
+print ("Content-type: text/html\r\n\r\n")
+print ("hello " + values['name'][0] + "!")
